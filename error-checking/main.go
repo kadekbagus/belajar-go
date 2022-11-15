4b4a35131cf6ce12667bced50ac1ccebbbb58c42
kadek <kadek@gotomalls.com>
2022-11-15 19:02:57 +0700

    add error checking function

--- error-checking/main.go
+++ error-checking/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -20,9 +21,32 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+func Devide(w http.ResponseWriter, r *http.Request) {
+	var x, y float32
+	x = 100.0
+	y = 0.0
+	f, err := devideValues(x, y)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+		return
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", x, y, f))
+}
+
+func devideValues(x, y float32) (float32, error) {
+	result := x / y
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+	return result, nil
+}
+
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/devide", Devide)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
