cce4981913d9d924e63484392c55aef25e9740c2
kadek <kadek@gotomalls.com>
2023-01-15 19:36:53 +0700

    prepare files for advance template cache

---
+++ template-cache-advance/cmd/web/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/kadekbagus/go-belajar/pkg/handlers"
+)
+
+const portNumber = ":8080"
+
+func main() {
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+
+	_ = http.ListenAndServe(portNumber, nil)
+
+	// go mod init myapp
+}
---
+++ template-cache-advance/go.mod
@@ -0,0 +1,3 @@
+module github.com/kadekbagus/go-belajar
+
+go 1.17
