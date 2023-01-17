ace7272eb602d6b2749baa8af5f5c20fdd78596e
kadek <kadek@gotomalls.com>
2023-01-17 17:49:00 +0700

    add application wide config for template

--- app-wide-config/cmd/web/main.go
+++ app-wide-config/cmd/web/main.go
@@ -2,20 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/kadekbagus/go-belajar/pkg/config"
 	"github.com/kadekbagus/go-belajar/pkg/handlers"
+	"github.com/kadekbagus/go-belajar/pkg/render"
 )
 
 const portNumber = ":8080"
 
 func main() {
+	var app config.AppConfig
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+	app.TemplateCache = tc
+
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
 	_ = http.ListenAndServe(portNumber, nil)
-
-	// go mod init myapp
 }
--- app-wide-config/pkg/render/renders.go
+++ app-wide-config/pkg/render/renders.go
@@ -9,9 +9,8 @@ import (
 )
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-
 	// create a template cache
-	tc, err := createTemplateCache()
+	tc, err := CreateTemplateCache()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -35,7 +34,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	}
 }
 
-func createTemplateCache() (map[string]*template.Template, error) {
+func CreateTemplateCache() (map[string]*template.Template, error) {
 	myCache := map[string]*template.Template{}
 
 	// get all of the files named *.page.tmpl from ./templates
